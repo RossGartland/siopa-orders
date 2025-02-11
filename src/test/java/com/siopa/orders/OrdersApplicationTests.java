1029bd07133bc4f9f19345605bc91f16776558c8
Gartland-R2 <Gartland-R2>
2025-02-11 10:43:03 +0000

    Initial setup

---
+++ src/test/java/com/siopa/orders/OrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siopa.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
